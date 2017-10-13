efc2bd9c4dd60352de441073fc89e399f2eda9b6
JosepRIvaille <josep.de.cid@gmail.com>
2017-10-13 23:24:05 +0200

    Add axios and first http get

--- web/src/index.js
+++ web/src/index.js
@@ -1,19 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+
+import axios from 'axios';
 
 import ButtonsBar from "./components/buttons_bar";
 import Description from "./components/description";
 import ImageSlider from "./components/image_slider";
 
-const App = () => {
-    
-    return (
-        <div>
-            <ImageSlider/>
-            <Description/>
-            <ButtonsBar/>
-        </div>
-    );
+class App extends Component {
+
+    API_PROFILE = 'http://ddragon.leagueoflegends.com/cdn/img/champion/splash/Yasuo_1.jpg';
+
+    constructor(props) {
+        super(props);
+        this.state = {};
+        this.getProfile();
+    }
+
+    getProfile() {
+        axios.get('API_PROFILE')
+            .then(function (profile) {
+                this.setState({
+                    profile: {
+                        description: 'Yasuo',
+                        images: [
+                            profile, profile
+                        ]
+                    }
+                });
+            })
+            .catch(function (err) {
+                console.error(err);
+            })
+    }
+
+    render() {
+        return (
+            <div>
+                <ImageSlider images={this.state.images}/>
+                <Description/>
+                <ButtonsBar/>
+            </div>
+        );
+    }
 };
 
 ReactDOM.render(<App/>, document.querySelector('#body'));
